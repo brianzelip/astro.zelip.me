ba5a9900571f8d34ca5fc6f79af9fc87ca5a69ab
Brian Zelip <brian.zelip@lyrasis.org>
2023-03-10 16:12:43 -0500

    Complete porting 11ty site to Astro
    Port palette.css and project styles to Tailwind.
    Add site settings via consts.ts and use in components.

---
+++ src/consts.ts
@@ -0,0 +1,9 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = `Brian Zelip`
+export const SITE_DESCRIPTION = `Brian Zelip's home page.`
+export const EMAIL = 'bzelip@protonmail.com'
+export const GH = 'https://github.com/brianzelip'
+export const TWITTER = 'https://twitter.com/bzelip'
+export const LINKEDIN = 'https://www.linkedin.com/in/zelip'
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,md}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,md}"],
+  theme: {
+    extend: {
+      screens: {
+        md: "832px", // 52em legacy Basscss vs. Tailwind's 768px
+      },
+    },
+  },
+  plugins: [],
+};
