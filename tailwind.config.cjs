34350529f3acb57c5831455169399f89c8512a06
Brian Zelip <brian.zelip@lyrasis.org>
2023-03-11 16:16:25 -0500

    Prettier syntax edits

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,md}"],
+  content: ['./src/**/*.{astro,html,md}'],
   theme: {
     extend: {
       screens: {
-        md: "832px", // 52em legacy Basscss vs. Tailwind's 768px
-      },
-    },
+        md: '832px' // 52em legacy Basscss vs. Tailwind's 768px
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
